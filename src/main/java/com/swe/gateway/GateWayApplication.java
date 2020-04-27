336ad6d8c012118e4467e085b686b20eef5ea1b1
papaya <1019424650@qq.com>
2020-04-28 01:12:40 +0800

    WebSocket修改，接口地址/websocket/{username} 前端发送数据的格式为 {"signal":10,"sensorId":1,"obsPropId":1} signal=0表示停止向当前客户端发送，>0表示前端希望获得sensorId=1，obsPropId=1的最新观测数据

--- src/main/java/com/swe/gateway/GateWayApplication.java
+++ src/main/java/com/swe/gateway/GateWayApplication.java
@@ -3,16 +3,24 @@ package com.swe.gateway;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * @author cbw
  */
-
+@EnableWebSocket
 @SpringBootApplication
 @MapperScan("com.swe.gateway.dao")
+
 public class GateWayApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(GateWayApplication.class, args);
+    }
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 }
