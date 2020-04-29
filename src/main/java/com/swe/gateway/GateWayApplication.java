f1ef2e7a0d01ad9480589000b90b00d02bf96ed4
Str0ngerCheng <2442799764@qq.com>
2020-04-29 13:33:11 +0800

    去掉 websocket相关配置

--- src/main/java/com/swe/gateway/GateWayApplication.java
+++ src/main/java/com/swe/gateway/GateWayApplication.java
@@ -3,14 +3,10 @@ package com.swe.gateway;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * @author cbw
  */
-@EnableWebSocket
 @SpringBootApplication
 @MapperScan("com.swe.gateway.dao")
 
@@ -18,9 +14,5 @@ public class GateWayApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(GateWayApplication.class, args);
-    }
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
     }
 }
