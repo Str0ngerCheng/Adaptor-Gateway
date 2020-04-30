db809e098648d380d2850f3774b98f05b1c5d6b2
papaya <1019424650@qq.com>
2020-04-30 22:27:23 +0800

    111

--- src/main/java/com/swe/gateway/service/NBIOTHandler.java
+++ src/main/java/com/swe/gateway/service/NBIOTHandler.java
@@ -4,48 +4,52 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.swe.gateway.dao.NBIOTRepository;
 import com.swe.gateway.model.NBIOT;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 /**
  * @author lx
  */
 @Component
 public class NBIOTHandler {
-    private final NBIOTRepository nbiotRepository;
 
-    public NBIOTHandler() {
-        nbiotRepository = new NBIOTRepository();
-    }
-    public Mono<String> insertNBIOTHandler(JSONObject jsonObject, String deviceID){
-        return Mono.just(jsonObject).flatMap(monosink->{
-            double temperature;
-            double humidity;
-            int timestamp=0;
-            //最外一层
-            //获取得是success,字符型;
-            jsonObject.getString("msg");
-            //获取得是0，字符型；
-            jsonObject.getString("code");
-            //第二层是数组
-            JSONArray dataList = jsonObject.getJSONArray("data");
-            double data[] = new double[100];
-            for (int j = 0; j < dataList.size(); j++) {
-                JSONObject list_obj =dataList.getJSONObject(j);
-                if (list_obj != null) {
-                    String item = list_obj.getString("item");
-                    String dtime = list_obj.getString("dtime");
-                    timestamp=Integer.parseInt(dtime);
-                    data[j] = Double.parseDouble(list_obj.getString("data"));
-                }
+    @Autowired
+    NBIOTRepository nbiotRepository;
 
+    public Mono<ServerResponse> insertNBIOTHandler(ServerRequest request) {
+        String deviceID = request.pathVariable("deviceID");
+        JSONObject jsonObject = JSONObject.parseObject(request.queryParams().toString());
+        double temperature;
+        double humidity;
+        int timestamp = 0;
+        //最外一层
+        //获取得是success,字符型;
+        jsonObject.getString("msg");
+        //获取得是0，字符型；
+        jsonObject.getString("code");
+        //第二层是数组
+        JSONArray dataList = jsonObject.getJSONArray("data");
+        double data[] = new double[100];
+        for (int j = 0; j < dataList.size(); j++) {
+            JSONObject list_obj = dataList.getJSONObject(j);
+            if (list_obj != null) {
+                String item = list_obj.getString("item");
+                String dtime = list_obj.getString("dtime");
+                timestamp = Integer.parseInt(dtime);
+                data[j] = Double.parseDouble(list_obj.getString("data"));
             }
-            temperature = data[0];
-            humidity = data[1];
-            NBIOT nbiot=new NBIOT(deviceID,temperature,humidity,timestamp);
-            nbiotRepository.insertNBIOT(nbiot);
-            return Mono.just(nbiot.toString());
-        });
+
+        }
+        temperature = data[0];
+        humidity = data[1];
+        NBIOT nbiot = new NBIOT(deviceID, temperature, humidity, timestamp);
+        nbiotRepository.insertNBIOT(nbiot);
+        return ServerResponse.ok().body(Mono.empty(), String.class);
 
     }
 }
---
+++ src/main/java/com/swe/gateway/service/RealTimeHandler.java
@@ -0,0 +1,108 @@
+package com.swe.gateway.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.core.io.JsonStringEncoder;
+import com.swe.gateway.model.Observation;
+import com.swe.gateway.util.WebSocketSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.reactive.socket.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import reactor.core.publisher.MonoSink;
+
+import java.sql.Date;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
+
+@Component
+public class RealTimeHandler implements WebSocketHandler {
+    @Autowired
+    private ConcurrentHashMap<String, WebSocketSender> senderMap;
+    //用了一个Map存储传感器的实时数据，RRALTIME_DATA只需要存每个设备的每个观测属性的最新一条数据就可以了
+    //key为sensorId+'_'+obsPropId组成的字符串
+    //前端点击发送一个传感器的设备id和一个观测属性id，然后去获取传感器的最新一条数据
+    //可以用http://coolaf.com/tool/chattest测试ws的接口
+    private static final ConcurrentHashMap<String, Observation> REALTIME_DATA = new ConcurrentHashMap<>();
+    //这个变量去判断是否继续向当前session持续发送消息
+    //不用interrupt()是因为在调用isinterrupted()判断之后标志位又会被置为true，另外这样可以控制向多个客户端的发送情况
+    private static final ConcurrentHashMap<String, Integer> SESSION_SIGNAL = new ConcurrentHashMap<>();
+    private static final Logger logger = LogManager.getLogger(RealTimeHandler.class.getName());
+
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+
+        String sessionid = session.getId();
+
+        //模拟实时数据
+        REALTIME_DATA.put("1_1", new Observation(1, 1, 2, 3, "11.5", Date.from(Instant.now())));
+        REALTIME_DATA.put("1_2", new Observation(1, 2, 2, 3, "22.5", Date.from(Instant.now())));
+        REALTIME_DATA.put("2_1", new Observation(2, 1, 2, 3, "33.5", Date.from(Instant.now())));
+
+        //获取到当前session的send函数的fluxsink对象并保存到senderMap里面
+        Mono<Void> output = session.
+                send(Flux.create(sink ->
+                        senderMap.put(sessionid, new WebSocketSender(session, sink))));
+
+        Mono<Void> input = session.receive()
+                .map(WebSocketMessage::getPayloadAsText)
+                .map(message -> {
+                    String info = "接收到客户端[" + sessionid + "]发送的数据：" + message;
+                    logger.info(info);
+                    //前端传回一个JSON对象，格式为{signal:0,sensorId:1,obsPropId:1}
+                    //接收到signal=0表示客户端暂停接收数据，signal=t表示前端希望每隔ts接收到服务端发送的数据
+                    JSONObject jsonObject = JSONObject.parseObject(message);
+                    int signal = (int) jsonObject.get("signal");
+                    String sensorId_obsPropId = jsonObject.getString("sensorId") + "_" + jsonObject.getString("obsPropId");
+                    if (signal <= 0) {
+                        SESSION_SIGNAL.put(sessionid, 0);
+                        logger.info("停止向客户端[" + sessionid + "]发送数据");
+                    } else {
+                        SESSION_SIGNAL.put(sessionid, 1);
+                        new Thread(() -> {while (SESSION_SIGNAL.get(sessionid) == 1) {
+                            Observation latestObs = REALTIME_DATA.get(sensorId_obsPropId);
+                            if (latestObs != null) {
+                                logger.info("服务端向客户端[" + sessionid + "]发送实时数据: " + latestObs.toString());
+                                //通过senderMap获取到当前session的sendsink发送数据
+                                senderMap.get(sessionid).sendData(JSONObject.toJSON(latestObs).toString());
+                            } else {
+                                logger.info("没有可用的实时数据 传感器_观测能力ID:" + sensorId_obsPropId);
+                                break;
+                            }
+                            try {
+                                Thread.sleep(signal * 1000);
+                            } catch (InterruptedException e) {
+                                logger.error(e.getMessage());
+                                e.printStackTrace();
+                            }
+                        }}).start();
+
+                    }
+                    return message;
+                }).then();
+        /**
+         * Mono.zip() 会将多个 Mono 合并为一个新的 Mono，任何一个 Mono 产生 error 或 complete 都会导致合并后的 Mono
+         * 也随之产生 error 或 complete，此时其它的 Mono 则会被执行取消操作。
+         * 这里由于是响应式，好像没有定义onopen和onclose的函数，全部都转发到了handle里面
+         */
+        return Mono.zip(output, input)
+                .doOnSubscribe(s->logger.info("客户端[" + sessionid + "]建立连接"))
+                .doOnError(s->logger.info("客户端[" + sessionid + "]发生错误"+s.getMessage()))
+                .doOnSuccess(s->logger.info("客户端[" + sessionid + "]关闭连接")).then();
+
+    }
+
+}
+
+
+
+
